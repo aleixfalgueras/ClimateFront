8478aded1b4bf6b4b4d7c2f89c4698d1e4326855
Aleix Falgueras <feina.aleix@gmail.com>
2020-03-22 13:00:36 +0100

    Scripts added.

---
+++ scripts/mongo/00.00.00/00_add_stock.js
@@ -0,0 +1,25 @@
+db.createCollection ("stock");
+
+db.stock.insertOne (
+    {
+        "id" : "2",
+        "name" : "tomatoe",
+        "quantity" : "0"
+    }
+);
+
+db.stock.insertOne (
+    {
+        "id" : "3",
+        "name" : "banana",
+        "quantity" : "0"
+    }
+);
+
+db.stock.insertOne (
+    {
+        "id" : "4",
+        "name" : "apple",
+        "quantity" : "0"
+    }
+);
