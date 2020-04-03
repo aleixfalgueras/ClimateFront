055094727340fba7b00f39319a214fe8d2a9e3ea
Aleix Falgueras <feina.aleix@gmail.com>
2020-04-03 15:16:47 +0200

    Refactor service structure, new function added: getCityId ().

--- src/services/openWeatherMap/OpenWeatherMap.py
+++ src/services/openWeatherMap/OpenWeatherMap.py
@@ -1,51 +1,75 @@
+import logging
+
 import requests
 
 from src import config
-from src.services.openWeatherMap.DayHourForecast import DayHourForecast
-from src.services.openWeatherMap.LocationForecast import LocationForecast
+from src.api_rest.model.entity.EntityDayHourForecast import EntityDayHourForecast
+from src.api_rest.model.entity.EntityLocationForecast import EntityLocationForecast
+
+
+### function: getCityId ###
+
+def getCityId (codeIATACity):
+    try:
+        if codeIATACity == "BCN": return "3128760"
+        elif codeIATACity == "LON": return "2643743"
+        else:
+            raise Exception ("Unknow city")
+
+    except Exception as exc :
+        logging.error ("OpenWeatherMap: getCityId: Error getting id of city with IATA code: " + codeIATACity)
+        logging.error ("[Exception: " + str (exc) + "]")
 
 
 ### function: getForecast ###
 
-def getForecast (city, days) : # free version: 5 days - 3 hours
-    apiCityCall = config.API_BASE_CALL + "&q=" + city
-    jsonResponse = requests.get (apiCityCall).json ()
+def getForecast (codeIATACity, days = 5, hours = 3) : # free version: 5 days - 3 hours
+    try:
 
-    forecasts = []
+        cityId = getCityId (codeIATACity)
 
-    for blockForecastInfo in jsonResponse ['list'] :
-        originalDate, hour = blockForecastInfo ['dt_txt'].split (' ')
+        apiCityCall = config.API_BASE_CALL + "&id=" + cityId
+        jsonResponse = requests.get (apiCityCall).json ()
 
-        date = originalDate.replace ("-", "")
-        hour = hour [:5]
+        forecasts = []
 
-        weather = blockForecastInfo ['weather'] [0] ['main']
-        weatherDescription = blockForecastInfo ['weather'] [0] ['description']
+        for blockForecastInfo in jsonResponse ['list'] :
+            originalDate, hour = blockForecastInfo ['dt_txt'].split (' ')
 
-        temperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp'] - 273)
-        minimTemperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp_min'] - 273)
-        maximTemperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp_max'] - 273)
+            date = originalDate.replace ("-", "")
+            hour = hour [:5]
 
-        pressure = str (blockForecastInfo ['main'] ['pressure'])
-        humidity = str (blockForecastInfo ['main'] ['humidity'])
+            weather = blockForecastInfo ['weather'] [0] ['main']
+            weatherDescription = blockForecastInfo ['weather'] [0] ['description']
 
-        windSpeed = str (blockForecastInfo ['wind'] ['speed'])
+            temperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp'] - 273)
+            minimTemperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp_min'] - 273)
+            maximTemperature = '{:.2f}'.format (blockForecastInfo ['main'] ['temp_max'] - 273)
 
-        forecasts.append (DayHourForecast (date,
-                                           hour,
-                                           weather,
-                                           weatherDescription,
-                                           temperature,
-                                           minimTemperature,
-                                           maximTemperature,
-                                           pressure,
-                                           humidity,
-                                           windSpeed))
+            pressure = str (blockForecastInfo ['main'] ['pressure'])
+            humidity = str (blockForecastInfo ['main'] ['humidity'])
 
-    latitude = str (jsonResponse ['city'] ['coord'] ['lat'])
-    longitude = str (jsonResponse ['city'] ['coord'] ['lon'])
-    country = jsonResponse ['city'] ['country']
-    cityName = jsonResponse ['city'] ['name']
-    timezone = str (jsonResponse ['city'] ['timezone'])
+            windSpeed = str (blockForecastInfo ['wind'] ['speed'])
 
-    return LocationForecast (latitude, longitude, country, cityName, timezone, forecasts)
+            forecasts.append (EntityDayHourForecast (date,
+                                                     hour,
+                                                     weather,
+                                                     weatherDescription,
+                                                     temperature,
+                                                     minimTemperature,
+                                                     maximTemperature,
+                                                     pressure,
+                                                     humidity,
+                                                     windSpeed))
+
+        latitude = str (jsonResponse ['city'] ['coord'] ['lat'])
+        longitude = str (jsonResponse ['city'] ['coord'] ['lon'])
+        country = jsonResponse ['city'] ['country']
+        cityName = jsonResponse ['city'] ['name']
+        timezone = str (jsonResponse ['city'] ['timezone'])
+
+        return EntityLocationForecast (latitude, longitude, country, cityName, timezone, forecasts)
+
+    except Exception as exc:
+        logging.error ("OpenWeatherMap: getForecast: Error getting forcasts for the city with IATA code: " + codeIATACity)
+        logging.error ("[Exception: " + str (exc) + "]")
