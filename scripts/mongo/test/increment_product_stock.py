36ab7bb90ac627319d5a09795f106ad00444fa7c
Aleix Falgueras <feina.aleix@gmail.com>
2020-04-08 13:23:22 +0200

    Test scripts deleted.

--- scripts/mongo/test/increment_product_stock.py
+++
@@ -1,3 +0,0 @@
-db.getCollection('product').updateOne({"id" : "2"}, {"$set" : {"quantity" : "10"}});
-db.getCollection('product').updateOne({"id" : "3"}, {"$set" : {"quantity" : "10"}});
-db.getCollection('product').updateOne({"id" : "4"}, {"$set" : {"quantity" : "10"}});
