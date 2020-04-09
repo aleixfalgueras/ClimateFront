0711e5a038a2c6d5df7433a40caa930ed1565f1a
Aleix Falgueras <feina.aleix@gmail.com>
2020-04-09 10:01:52 +0200

    Log the message too.

--- src/opcua_communication/subscription/SubscriptionHandler.py
+++ src/opcua_communication/subscription/SubscriptionHandler.py
@@ -1,3 +1,5 @@
+import logging
+
 ################################################################################
 # class: SubscriptionHandler
 ################################################################################
@@ -7,4 +9,7 @@ class SubscriptionHandler () :
     ### function: datachange_notification ###
 
     def datachange_notification (self, node, val, data) :
-        print ("Node:", node, "Value:", val, "Data:", data)
+        message = "Node:", node, "Value:", val, "Data:", data
+
+        logging.info (message)
+        print (message)
